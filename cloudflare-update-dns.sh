2c9d2c61c013538ad5e67b10344d2b2bc8922ab3
bugiii <bugiii@gmail.com>
2022-04-19 08:56:29 +0900

    change dns updater to be easier to use

--- cloudflare-update-dns.sh
+++ cloudflare-update-dns.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# ./oci-free PROFILE ip-list | awk -F, '{ print $2 " " $3 }' | xargs -L1 ./cloudflare-update-dns.sh
+# ./oci-free PROFILE ip-list | xargs -P8 -L1 ./cloudflare-update-dns.sh
+# ./oci-free PROFILE ipv6-list | xargs -P8 -L1 ./cloudflare-update-dns.sh
 
 DOMAIN=somewhere.com
 AUTH_EMAIL=example@example.com
@@ -9,27 +10,33 @@ ZONE_ID=CF_Zone_ID
 
 source ".cloudflare-update-dns.conf"
 
-A_RECORD_NAME=$1
-PUBLIC_IP=$2
-A_RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$A_RECORD_NAME.$DOMAIN" \
-	-H "Content-Type: application/json" \
-	-H "X-Auth-Email: $AUTH_EMAIL" \
-	-H "Authorization: Bearer $API_KEY" | jq -r '{"result"}[] | .[0] | .id')
+RECORD_TYPE=$1
+RECORD_NAME=$2
+PUBLIC_IP=$3
 
-# Record the new public IP address on Cloudflare using API v4
-RECORD=$(cat <<EOF
-{ "type": "A",
-  "name": "$A_RECORD_NAME",
+BASE_URI="https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records"
+GET_ID_URI="$BASE_URI?type=$RECORD_TYPE&name=$RECORD_NAME.$DOMAIN"
+HEADERS=(-H "Content-Type: application/json" \
+	-H "X-Auth-Email: $AUTH_EMAIL" \
+	-H "Authorization: Bearer $API_KEY")
+RECORD_DATA=$(cat <<EOF
+{ "type": "$RECORD_TYPE",
+  "name": "$RECORD_NAME",
   "content": "$PUBLIC_IP",
   "ttl": 180,
   "proxied": false }
 EOF
 )
-curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$A_RECORD_ID" \
-     -X PUT \
-     -H "Content-Type: application/json" \
-     -H "X-Auth-Email: $AUTH_EMAIL" \
-     -H "Authorization: Bearer $API_KEY" \
-     -d "$RECORD"
 
-# TODO: create record if not exist
+RECORD_ID=$(curl -s -X GET "$GET_ID_URI" "${HEADERS[@]}" | jq -r '{"result"}[] | .[0] | .id')
+
+# create record if not exist
+if [ "$RECORD_ID" == "null" ]; then
+	RECORD_ID=$(curl -s -X POST "$BASE_URI" "${HEADERS[@]}" -d "$RECORD_DATA" | jq -r '.result.id')
+fi
+
+ID_URI="$BASE_URI/$RECORD_ID"
+RESULT=$(curl -s -X PUT "$ID_URI" "${HEADERS[@]}" -d "$RECORD_DATA" | jq -r '.result.content')
+
+echo $RECORD_TYPE $RECORD_NAME $PUBLIC_IP $RESULT
+
