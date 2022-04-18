387603de0a6516e38d1a669e3b1b2a180e646efa
TaeKyeong Wang <bugiii@gmail.com>
2022-04-19 01:34:23 +0900

    fix cloudflare dns updater

--- cloudflare-update-dns.sh
+++ cloudflare-update-dns.sh
@@ -2,26 +2,19 @@
 
 # ./oci-free PROFILE ip-list | awk -F, '{ print $2 " " $3 }' | xargs -L1 ./cloudflare-update-dns.sh
 
+DOMAIN=somewhere.com
 AUTH_EMAIL=example@example.com
 API_KEY=CF_Authorization_key
 ZONE_ID=CF_Zone_ID
 
 source ".cloudflare-update-dns.conf"
 
-#echo $AUTH_EMAIL
-#echo $AUTH_KEY
-#echo $ZONE_ID
-
 A_RECORD_NAME=$1
 PUBLIC_IP=$2
-A_RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$A_RECORD_NAME" \
-	     -H "Host: api.cloudflare.com" \
-	          -H "User-Agent: ddclient/3.9.0" \
-		       -H "Connection: close" \
-     -H "X-Auth-Email: $AUTH_EMAIL" \
-     -H "Authorization: Bearer $API_KEY" \
-     -H "Content-Type: application/json")
-echo $A_RECORD_ID
+A_RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$A_RECORD_NAME.$DOMAIN" \
+	-H "Content-Type: application/json" \
+	-H "X-Auth-Email: $AUTH_EMAIL" \
+	-H "Authorization: Bearer $API_KEY" | jq -r '{"result"}[] | .[0] | .id')
 
 # Record the new public IP address on Cloudflare using API v4
 RECORD=$(cat <<EOF
